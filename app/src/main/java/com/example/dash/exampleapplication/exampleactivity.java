c2d252af2d893c2b1cabf676b0516ab6090714fe
dash <rdpmail1@gmail.com>
2017-04-15 23:09:13 +0530

    Inital Commit

---
+++ app/src/main/java/com/example/dash/exampleapplication/exampleactivity.java
@@ -0,0 +1,13 @@
+package com.example.dash.exampleapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class exampleactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exampleactivity);
+    }
+}
